b5ff634e39d69b9507f550160bab62c5f74a9147
David C <david.contreras@gorillalogic.com>
2022-11-16 09:15:45 -0600

    dependency injection lib

---
+++ Azure/Functions/ApiFunctions/ApiFunctions/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: FunctionsStartup(typeof(ApiFunctions.Startup))]
+namespace ApiFunctions
+{
+	public class Startup : FunctionsStartup
+	{
+		public override void Configure(IFunctionsHostBuilder builder)
+		{
+			builder.Services.AddHttpClient();
+		}
+	}
+}
