d9e0a8c028bd817c1e5a9f1a2224907a16c064e8
davidVM <dcontre10@gmail.com>
2023-12-03 23:15:06 -0600

    remove redundant app file override

--- WebApiSolution/MyFinanceWebApi/MyFinanceWebApiCore/Program.cs
+++ WebApiSolution/MyFinanceWebApi/MyFinanceWebApiCore/Program.cs
@@ -24,11 +24,6 @@ namespace MyFinanceWebApiCore
 					logging.ClearProviders();
 					logging.AddConsole();
 				})
-				.ConfigureAppConfiguration((hostingContext, config) =>
-				{
-					config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-						  .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
-				})
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
