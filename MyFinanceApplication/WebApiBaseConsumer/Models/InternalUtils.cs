dd5634c30720a1a772af404ec3a8d61aedcc57f5
davidcontreras10 <dcontre10@gmail.com>
2023-12-21 20:04:30 -0600

    Feature/web api base consumer lib (#28)
    * remove dependencies
    
    * add package info
    
    * use nuget package instead of project reference

---
+++ MyFinanceApplication/WebApiBaseConsumer/Models/InternalUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace WebApiBaseConsumer.Models
+{
+	internal static class CustomSerializationInfoExtensions
+	{
+		public static bool ExistsKey(this SerializationInfo info, string key, bool ignoreCase = false)
+		{
+			foreach (var serializationEntry in info)
+			{
+				var comparison = ignoreCase
+					? StringComparison.InvariantCultureIgnoreCase
+					: StringComparison.InvariantCulture;
+				if (string.Compare(serializationEntry.Name, key, comparison) == 0)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		internal static SerializationEntry? GetSerializationEntry(this SerializationInfo info, string key,
+			bool ignoreCase = false)
+		{
+			foreach (var serializationEntry in info)
+			{
+				var comparison = ignoreCase
+					? StringComparison.InvariantCultureIgnoreCase
+					: StringComparison.InvariantCulture;
+				if (string.Compare(serializationEntry.Name, key, comparison) == 0)
+				{
+					return serializationEntry;
+				}
+			}
+
+			return null;
+		}
+	}
+}
