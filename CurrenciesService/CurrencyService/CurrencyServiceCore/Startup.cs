da15f36f5e5012819d0f34c9f097c724bb4e573a
David Contreras Zúñiga <dcontre10@gmail.com>
2024-10-27 02:14:36 -0600

    currency service to docker

--- CurrenciesService/CurrencyService/CurrencyServiceCore/Startup.cs
+++ CurrenciesService/CurrencyService/CurrencyServiceCore/Startup.cs
@@ -4,18 +4,11 @@ using Domain.Repositories;
 using Domain.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CurrencyServiceCore
 {
---
+++ CurrenciesService/CurrencyService/Domain/Models/ExchangeRateResultModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+	public class ExchangeRateResultModel
+	{
+		public IEnumerable<MethodParam> MethodIds { get; set; }
+		public DateTime DateTime { get; set; }
+
+		public class MethodParam
+		{
+			public int Id { get; set; }
+			public bool IsPurchase { get; set; }
+			public DateTime? DateTime { get; set; }
+		}
+	}
+}
